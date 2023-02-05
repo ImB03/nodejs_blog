dddbc31ffee41b03c8fa22723e6e9314daa8f16b
ImB03 <giabao7803@gmail.com>
2023-02-05 19:41:24 +0700

    27. [CRUD] Read from DB

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const newsRouter = require('./news');
 const siteRouter = require('./site');
 
 function route(app) {
-    app.use('/news', newsRouter);
-    app.use('/', siteRouter);
+    app.get('/news', newsRouter);
+    app.get('/', siteRouter);
 }
 module.exports = route;
